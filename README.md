140d493e4cd0536d7fb6322081dda198e27e4c44
liangshen001 <wangliang520wl@hotmail.com>
2022-05-15 19:08:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+## @liangshen/alfred-jetbrains
+
+*Alfred工作流，快速启动Jetbrains系软件中打开过的项目*
+*Alfred workflow for Jetbrains*
+
+### 运行环境
+
+* MacOS
+* Nodejs 14.x.x
+* Alfred Powerpack
+* Jetbrains (Webstorm Idea Pycharm)
+
+### 安装
+
+```
+npm i -g @liangshen/alfred-jenkins
+```
+
+### 使用方法
+
+目前只支持Webstorm Idea Pycharm三种IDE
+
+可使用 option + W 快捷键调出Webstorm工作流搜索框(或者调出alfred搜索框输入关键字 webstorm)
+
+回车可使用Webstorm打开项目, 按住Command键回车可在Finder中打开项目
+
+![](./docs/webstorm.png)
+![](./docs/webstorm2.png)
+
+可使用 option + I 快捷键调出Idea工作流搜索框(或者调出alfred搜索框输入关键字 idea)
+
+回车可使用Idea打开项目, 按住Command键回车可在Finder中打开项目
+
+![](./docs/idea.png)
+![](./docs/idea2.png)
+可使用 option + P 快捷键调出Pycharm工作流搜索框(或者调出alfred搜索框输入关键字 pycharm)
+
+回车可使用Pycharm打开项目, 按住Command键回车可在Finder中打开项目
+![](./docs/pycharm.png)
+![](./docs/pycharm2.png)
+
+
---
+++ index.js
@@ -0,0 +1,34 @@
+import alfy from 'alfy';
+import fs from "fs";
+import os from "os";
+import {xml2js} from "xml-js";
+
+const preferencesBasePath = `${os.homedir()}/Library/Application Support/JetBrains/`;
+
+export async function invoke(app) {
+    const folders = fs.readdirSync(preferencesBasePath);
+    const appDir = folders.filter(name => name.startsWith(app)).reduce((p, v) => (p && p > v) ? p : v)
+    if (appDir) {
+        const recentPreferencesFile = `${preferencesBasePath}${appDir}/options/recentProjects.xml`;
+        const recentPreferences = fs.readFileSync(recentPreferencesFile, {encoding: 'utf8'});
+        const recentPreferencesObj = xml2js(recentPreferences)
+        const items = recentPreferencesObj.elements.find(e => e.name === 'application')
+            .elements.find(e => e.name === 'component')
+            .elements.find(e => e.name === 'option' && e.attributes.name === 'additionalInfo')
+            .elements.find(e => e.name === 'map')
+            .elements.map(e => {// .filter(e => e.attributes.opened !== 'true')
+                const arg = e.attributes.key.replace('$USER_HOME$', os.homedir());
+                const recentProjectMetaInfo = e.elements.find(e => e.name === 'value')
+                    .elements.find(e => e.name === 'RecentProjectMetaInfo')
+                const title = recentProjectMetaInfo
+                    .attributes.frameTitle?.split(' – ')[0] || arg.substring(arg.lastIndexOf('/') + 1);
+                return {
+                    arg,
+                    title,
+                    subtitle: arg,
+                    show: recentProjectMetaInfo.attributes.opened !== 'true'
+                }
+            }).filter(i => i.show && i.title.toLowerCase().includes(alfy.input.toLowerCase()));
+        alfy.output(items.reverse());
+    }
+}
